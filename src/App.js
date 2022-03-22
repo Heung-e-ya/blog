5583849b06cdbba1a24ce8b2183eeccb3024113e
heung <hoonun@gmail.com>
2022-03-22 13:46:16 +0900

    react 수업1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+/* eslint-disable */
+import React , { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','남자 체형별 패션', '강남 맛집', '코로나 거리두기 단계']);
+  
+
+  let posts = '강남 고기 맛집';
+  const [counter, setCounter] = React.useState(0);
+  const onClick =() =>{
+    
+    setCounter((current) => current + 1);
+    
+  }
+  const 제목바꾸기 =()=>{
+    var newArray = [...글제목];
+    newArray[0] ='여자 코트 추천'
+    글제목변경(newArray);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <button onClick={제목바꾸기}>여자</button>
+        <h3> { 글제목[0] } <span onClick={onClick}>👍</span>{counter}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[1] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[2] }</h3>
+        <p>2월 1일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[3] }</h3>
+        <p>2월 21일 발행</p>
+        <hr/>
+      </div>
+      
+      <div className='modal'>
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
     </div>
   );
 }
